739c28669c2a76036ad5b1e1437043c011c1370d
TDDavid <td.tanko.david@gmail.com>
2024-07-03 21:34:36 +0200

    Enable 8-way movement. Adjust speed by process delta.

--- SnakeGame.gd
+++ SnakeGame.gd
@@ -7,17 +7,16 @@ var movement_direction = Vector2.UP
 var snakeBodyInstance = SnakeBody.instantiate()
 var foodInstance = Food.instantiate()
 
-var snake_speed = 2
+var snake_speed = 200
 
 func spawn_food():
 	var food_x = randi_range(100, 500)
 	var food_y = randi_range(100, 500)
 	foodInstance.position = Vector2(food_x, food_y)
-	#add_child(foodInstance)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#foodInstance.food_eaten.connect(consume_food)
+	#foodInstance.area_entered.connect(consume_food)
 	
 	spawn_food()
 	add_child(foodInstance)
@@ -26,30 +25,28 @@ func _ready():
 	add_child(snakeBodyInstance)
 
 func consume_food():
-	print("test slot")
-	#remove_child(foodInstance)
+	remove_child(foodInstance)
 	spawn_food()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#print("FPS %d" % Engine.get_frames_per_second())
 	handle_input()
 	update_position(delta)
 
 func handle_input():
-	if Input.is_action_pressed("ui_up"):
-		movement_direction = Vector2.UP
-	elif Input.is_action_pressed("ui_down"):
-		movement_direction = Vector2.DOWN
-	elif Input.is_action_pressed("ui_left"):
-		movement_direction = Vector2.LEFT
-	elif Input.is_action_pressed("ui_right"):
-		movement_direction = Vector2.RIGHT
+	movement_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		
+	movement_direction = movement_direction.normalized()
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		foodInstance.print_tree_pretty()
+		snakeBodyInstance._on_area_entered(foodInstance.get_child(1))
 	
 	if Input.is_action_pressed("ui_custom_run"):
-		snake_speed = 10
+		snake_speed = 500
 	elif Input.is_action_just_released("ui_custom_run"):
-		snake_speed = 2
+		snake_speed = 200
 
 func update_position(delta):
-	#snakeBodyInstance.position += movement_direction * snake_speed
-	snakeBodyInstance.move_snake(movement_direction, snake_speed)
+	snakeBodyInstance.move_snake(movement_direction, snake_speed * delta)
