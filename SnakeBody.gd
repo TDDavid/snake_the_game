26dd4372759114369174c06b5a6fe49a8931591d
TDDavid <td.tanko.david@gmail.com>
2024-07-03 20:29:30 +0200

    Keep the snake in the viewport.

--- SnakeBody.gd
+++ SnakeBody.gd
@@ -2,8 +2,26 @@ extends Node2D
 
 var SnakeBodyPart = preload("res://SnakeBodyPart.tscn")
 var tail_segments = []
+var viewport_rect = Vector2()
+
+func is_position_inside_window(new_position: Vector2) -> bool:
+	return new_position.x >= 0 and new_position.y >= 0 and \
+		   new_position.x < viewport_rect.size.x and new_position.y < viewport_rect.size.y
+
+func move_to_opposite_side_of_window(new_position: Vector2) -> Vector2:
+	var window_width = viewport_rect.size.x
+	var window_height = viewport_rect.size.y
+	
+	var dx = window_width - new_position.x
+	var dy = window_height - new_position.y
+	
+	var new_x = 0 if dx <= 0 else window_width if dx >= window_width else new_position.x
+	var new_y = 0 if dy <= 0 else window_height if dy >= window_height else new_position.y
+	
+	return Vector2(new_x, new_y)
 
 func move_snake(move_direction: Vector2, move_speed: int):
+	viewport_rect = get_viewport().get_visible_rect()
 	var move_vec = move_direction * move_speed
 	
 	var arrayinv = tail_segments.duplicate()
@@ -15,7 +33,13 @@ func move_snake(move_direction: Vector2, move_speed: int):
 		else:
 			arrayinv[segmentIdx].position = arrayinv[segmentIdx + 1].position
 			
-	position += move_vec
+	var new_position = position + move_vec
+	
+	if !is_position_inside_window(new_position):
+		position = move_to_opposite_side_of_window(new_position)
+		print(position)
+	else:
+		position = new_position
 
 func _ready():
 	$Area2D.area_entered.connect(_on_area_entered)
